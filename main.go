4ad960668fea0d132f69e4a4dcbfb9a57227b482
Devshara <developer.ishara@gmail.com>
2023-09-11 16:18:47 +0530

    delete product

--- main.go
+++ main.go
@@ -59,11 +59,45 @@ func createProduct(c *gin.Context) {
 	c.JSON(200, newProduct)
 }
 
+// func updateProductPrice(c *gin.Context) {
+// 	id, ok := c.GetQuery("id")
+// 	price, ok := c.GetQuery("price")
+
+// 	if ok == false {
+// 		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id query param"})
+// 		return
+// 	}
+
+// 	product, err := getProductById(id)
+
+// 	if err != nil {
+// 		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Product not found"})
+// 		return
+// 	}
+
+// 	product.Price = price
+// }
+
+func DeleteProduct(c *gin.Context) {
+
+	id := c.Param("id")
+
+	for i, p := range products {
+		if p.ID == id {
+
+			products[i] = products[len(products)-1] // Copy last element to index i.
+			products[len(products)-1] = product{}   // Erase last element (write zero value).
+			products = products[:len(products)-1]
+		}
+	}
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/api", getProducts)
 	router.GET("/api/:id", productById)
 	router.POST("/api", createProduct)
+	router.DELETE("/api/:id", DeleteProduct)
 	router.Run("localhost:3000")
 
 }
